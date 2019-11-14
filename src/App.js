4983d218a334edc266b2fa03df5abd380fd6a4fe
mandyshen <1099232082@qq.com>
2019-11-14 22:18:08 +0800

    登陆注册

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Admin from "./pages/admin/admin";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
-import Login from './pages/login/Login';
-import Admin from './pages/admin/admin';
 /**
  * 应用根组件
  */
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-    }
+    this.state = {};
   }
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route path='/login' component={Login}></Route>
-          <Route path='/' component={Admin}></Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
+          <Route path="/" component={Admin}></Route>
         </Switch>
       </BrowserRouter>
-    )
+    );
   }
 }
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { Redirect, Link } from "react-router-dom";
 import { Form, Icon, Input, Button, Message, Checkbox } from "antd";
-import "./login.less";
+import "../login/login.less";
 import logo from "../../assets/images/logo.jpg";
 import memoryUtils from "../../utils/memoryUtils";
 import API from "../../api/api";
 
-class Login extends Component {
+class Register extends Component {
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -16,27 +16,22 @@ class Login extends Component {
           username: values.username,
           password: values.password
         };
-        API.doctorLogin({ param })
+        API.doctorRegister({ param })
           .then(res => {
-            console.log(res);
             if (res.code !== 200) {
-              Message.error("登录失败，用户名或密码错误！");
+              Message.error("注册失败！");
             } else {
-              Message.success("登录成功！");
-              this.props.history.push("/");
+              Message.success("注册成功！");
+              this.props.history.push("/login");
             }
           })
           .catch(err => {
-            Message.error("登录失败！请重试！");
+            Message.warning("注册失败！");
           });
       }
     });
   };
   render() {
-    const user = memoryUtils.user;
-    if (user && user._id) {
-      return <Redirect to="/" />;
-    }
     const { getFieldDecorator } = this.props.form;
     return (
       <div className="login">
@@ -48,7 +43,7 @@ class Login extends Component {
             </div>
           </div>
           <div className="login-right">
-            <h2>用户登录</h2>
+            <h2>用户注册</h2>
             <Form onSubmit={this.handleSubmit} className="login-form">
               <Form.Item>
                 {getFieldDecorator("username", {
@@ -76,23 +71,18 @@ class Login extends Component {
                 )}
               </Form.Item>
               <Form.Item>
-                {getFieldDecorator("remember", {
-                  valuePropName: "checked",
-                  initialValue: true
-                })(<Checkbox className="login-form-check">记住我</Checkbox>)}
-                <Link className="login-form-forgot" to="/login">
-                  忘记密码
-                </Link>
                 <Button
                   type="primary"
                   block
                   htmlType="submit"
                   className="login-form-button"
                 >
-                  登录
+                  注册
                 </Button>
-                <span style={{ color: "white", marginRight: "5px" }}>或</span>
-                <Link to="/register">现在注册!</Link>
+                <span style={{ color: "white", marginRight: "5px" }}>
+                  或已存在账号
+                </span>
+                <Link to="/login">现在登录!</Link>
               </Form.Item>
             </Form>
           </div>
@@ -102,5 +92,4 @@ class Login extends Component {
     );
   }
 }
-const WrappedNormalLoginForm = Form.create({ name: "normal_login" })(Login);
-export default WrappedNormalLoginForm;
+export default Form.create()(Register);
